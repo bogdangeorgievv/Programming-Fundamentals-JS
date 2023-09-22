565cc34c95b0b14a5b296f2762a142e7343cce96
Bogdan Georgiev <bobogeorgievv97@gmail.com>
2023-09-22 12:39:53 +0300

    Create 1_echo-type.js

---
+++ Data Types and Variables/LAB/1_echo-type.js
@@ -0,0 +1,13 @@
+function solve(param) {
+    let type = typeof param;
+    console.log(type)
+
+  if (typeof param == "string" || typeof param == "number") {
+    
+    console.log(param);
+  } else {
+    
+    console.log("Parameter is not suitable for printing");
+  }
+}
+solve('Hello JavaScript')
