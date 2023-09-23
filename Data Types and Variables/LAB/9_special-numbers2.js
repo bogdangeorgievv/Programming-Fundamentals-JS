11f9b2caf3c686b1aa6fe0934ea4f979aebcf51d
Bogdan Georgiev <bobogeorgievv97@gmail.com>
2023-09-23 12:15:36 +0300

    Create 9_special-numbers2.js

---
+++ Data Types and Variables/LAB/9_special-numbers2.js
@@ -0,0 +1,17 @@
+function solve(n) {
+  let sum = 0;
+
+  for (let i = 1; i <= n; i++) {
+    i = i.toString();
+    for (let j = 0; j < i.length; j++) {
+      sum += Number(i[j]);
+    }
+    if (sum == 5 || sum == 7 || sum == 11) {
+      console.log(`${i} -> True`);
+    } else {
+      console.log(`${i} -> False`);
+    }
+    sum = 0;
+  }
+}
+solve(15);
