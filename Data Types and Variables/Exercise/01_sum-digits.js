90c23753ec51579206cae7a2bac9ce34c2b97e7b
Bogdan Georgiev <140177649+bogdangeorgievv@users.noreply.github.com>
2023-09-24 20:49:48 +0300

    Create 01_sum-digits.js

---
+++ Data Types and Variables/Exercise/01_sum-digits.js
@@ -0,0 +1,10 @@
+function solve(num) {
+    let numAsString = String(num);
+    let sum = 0;
+
+    for(let i = 0; i < numAsString.length; i++) {
+        sum += Number(numAsString[i]);
+    }
+    console.log(sum)
+}
+solve(245678)
