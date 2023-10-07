62f90dde8fb2923a4ecaaef2026a0449f11d7f38
Bogdan Georgiev <140177649+bogdangeorgievv@users.noreply.github.com>
2023-10-07 11:54:17 +0300

    Create 02_day-of-the-week.js

---
+++ Arrays/LAB/02_day-of-the-week.js
@@ -0,0 +1,11 @@
+function solve(num) {
+    let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    if (num >=1 && num<=7) {
+        console.log(days[num - 1]);
+    } else {
+        console.log('Invalid day!');
+    }
+}
+solve(3)
+solve(4)
+solve(9)
