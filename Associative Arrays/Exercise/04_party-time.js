35e5211b3807299fd9ccdd1bcbfdaa7573ce9aa4
Bogdan Georgiev <140177649+bogdangeorgievv@users.noreply.github.com>
2023-11-27 18:41:41 +0200

    Create 04_party-time.js

---
+++ Associative Arrays/Exercise/04_party-time.js
@@ -0,0 +1,26 @@
+function creatingPartyList(input) {
+    let vips = [];
+    let regulars = [];
+    let person = input.shift();
+    while (person != 'PARTY') {
+        if (isNaN(Number(person[0]))) {
+            regulars.push(person);
+        } else {
+            vips.push(person);
+        }
+        person = input.shift();
+    }
+    for (const comingGuest of input) {
+        if (vips.includes(comingGuest)) {
+            vips.splice(vips.indexOf(comingGuest), 1);
+        }
+        if (regulars.includes(comingGuest)) {
+            regulars.splice(regulars.indexOf(comingGuest), 1);
+        }
+    }
+    console.log(vips.concat(regulars).length);
+    vips.concat(regulars).forEach(guest => {
+        console.log(guest);
+    });
+}
+creatingPartyList(['7IK9Yo0h','9NoBUajQ','Ce8vwPmE', 'SVQXQCbc', 'tSzE5t0p', 'PARTY', '9NoBUajQ', 'Ce8vwPmE', 'SVQXQCbc' ])
